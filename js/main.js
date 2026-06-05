ac0c554ad978ec687375729d0c8794b7cfc5eb11
alexanderpollard <76454995+alexanderpollard@users.noreply.github.com>
2026-06-05 18:19:57 +0000

    made header to be less code using javascript because php doesnt work for static sites

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ if (hamburgerBtn && navLinks) {
         hamburgerBtn.setAttribute('aria-expanded', String(!isExpanded));
     });
 }
+
+fetch('php/header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    });
